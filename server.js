d3e147f948a34e5c2418426b862b6f0fefe34922
taohansi <hans-tao@qq.com>
2021-09-20 11:02:30 +0800

    add json response

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('hello world\n')
+  const {url, method} = req  
+  if (url === '/hello' && method === 'GET') {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello world\n')
+    } else if (url === '/json' && method === 'GET') {
+      res.writeHead(200, {'Content-Type':'application/json'})
+      res.end(JSON.stringify({data : 'json'}))
+    }
+    
   })
   
   server.listen(3000, () => {
