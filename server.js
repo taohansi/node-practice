9bbdf1eb4b99abf5459d96c9b7d50d1204f5f473
taohansi <hans-tao@qq.com>
2021-09-20 10:47:04 +0800

    init server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('hello world\n')
+  })
+  
+  server.listen(3000, () => {
+    console.log('server start')
+  })
